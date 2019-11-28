12a52042d15ce04d655ce9ab7b5d503518fe1a02
dhruvinparikh <dparikh@apigarage.com>
2019-11-28 15:21:19 -0500

    `WIP` creating storage value, store value and store mapping

--- node/runtime/src/substratekitties.rs
+++ node/runtime/src/substratekitties.rs
@@ -1,15 +1,23 @@
-use support::{decl_storage, decl_module};
+use support::{decl_storage, decl_module, StorageMap, dispatch::Result};
+use system::ensure_signed;
 
-pub trait Trait: system::Trait {}
+pub trait Trait: balances::Trait {}
 
 decl_storage! {
     trait Store for Module<T: Trait> as KittyStorage {
-        // Declare storage and getter functions here
+        Value: map T::AccountId => u64;
     }
 }
 
 decl_module! {
     pub struct Module<T: Trait> for enum Call where origin: T::Origin {
-        // Declare public functions here
+
+        fn set_value(origin, value: u64) -> Result {
+            let sender = ensure_signed(origin)?;
+
+            <Value<T>>::insert(sender, value);
+
+            Ok(())
+        }
     }
 }

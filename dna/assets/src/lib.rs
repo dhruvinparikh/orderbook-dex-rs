e8c1dc12bff56b07df8432bc312c0dc5862e05f5
dhruvinparikh <dparikh@apigarage.com>
2019-12-03 13:53:13 -0500

    `WIP` katalchain's asset

--- dna/assets/src/lib.rs
+++ dna/assets/src/lib.rs
@@ -1,241 +1,74 @@
-// Copyright 2019 Liebi Technologies.
-// This file is part of Bifrost.
-
-// Bifrost is free software: you can redistribute it and/or modify
+// Copyright 2019 by Trinkler Software AG (Switzerland).
+// This file is part of the Katal Chain.
+//
+// Katal Chain is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-
-// Bifrost is distributed in the hope that it will be useful,
+// (at your option) any later version <http://www.gnu.org/licenses/>.
+//
+// Katal Chain is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
 
-// You should have received a copy of the GNU General Public License
-// along with Bifrost.  If not, see <http://www.gnu.org/licenses/>.
+//! # Assets module
+//!
 
-// Ensure we're `no_std` when compiling for Wasm.
 #![cfg_attr(not(feature = "std"), no_std)]
+// The above line is needed to compile the Wasm binaries.
 
-use rstd::prelude::*;
-use codec::{Encode, Decode};
-use srml_support::{StorageValue, StorageMap, Parameter,
-	decl_module, decl_event, decl_storage, ensure};
-use sr_primitives::traits::{Member, SimpleArithmetic, One, Zero, StaticLookup};
-use system::{ensure_signed, ensure_root};
-// use node_primitives::{ClearingHandler, AssetIssue};
+// Importing crates declared in the cargo.toml file.
+use codec::{Decode, Encode};
+use primitives::H256;
+use structures::Real;
+use support::{decl_module, decl_storage, dispatch::Result, StorageMap};
 
-mod mock;
-mod tests;
+// Importing the rest of the files in this crate.
+mod burn;
+mod mint;
+mod transfer;
+use burn::*;
+use mint::*;
+use transfer::*;
 
-#[derive(Encode, Decode, Default, Clone, Eq, PartialEq, Debug)]
-pub struct Token<Balance> {
-	symbol: Vec<u8>,
-	precision: u16,
-	pub total_supply: Balance,
+// This module's configuration trait.
+pub trait Trait: system::Trait {}
+
+// This module's storage items.
+decl_storage! {
+    trait Store for Module<T: Trait> as AssetsStorage {
+        pub Balances get(fn balances): map (u32, H256) => Real;
+        pub TotalSupply get(fn total_supply): map u32 => Real;
+    }
 }
 
-/// The module configuration trait.
-pub trait Trait: system::Trait {
-	/// The overarching event type.
-	type Event: From<Event<Self>> + Into<<Self as system::Trait>::Event>;
+// This module's dispatchable functions.
+decl_module! {
+    // The module declaration.
+    pub struct Module<T: Trait> for enum Call where origin: T::Origin {
 
-	/// The units in which we record balances.
-	type Balance: Member + Parameter + SimpleArithmetic + Default + Copy + Zero + From<Self::BlockNumber>;
+        pub fn dispatch_transfer(origin,from_address: H256, to_address: H256, asset_id: u32, amount: Real) -> Result {
+            // Call corresponding internal function.
+            Self::transfer(from_address, to_address, asset_id, amount)?;
 
-	/// The arithmetic type of asset identifier.
-	type AssetId: Member + Parameter + SimpleArithmetic + Default + Copy;
+            // Return Ok if successful.
+            Ok(())
+        }
 
-	// Clearing handler for assets change
-	// type ClearingHandler: ClearingHandler<Self::AssetId, Self::AccountId, Self::BlockNumber, Self::Balance>;
+        pub fn dispatch_mint(origin, to_address: H256, asset_id: u32, amount: Real) -> Result {
+            // Call corresponding internal function.
+            Self::mint(to_address, asset_id, amount)?;
+
+            // Return Ok if successful.
+            Ok(())
+        }
+
+        pub fn dispatch_burn(origin,from_address: H256, asset_id: u32, amount: Real) -> Result {
+            // Call corresponding internal function.
+            Self::burn(from_address, asset_id, amount)?;
+
+            // Return Ok if successful.
+            Ok(())
+        }
+    }
 }
-
-decl_event!(
-	pub enum Event<T>
-		where <T as system::Trait>::AccountId,
-			<T as Trait>::Balance,
-			<T as Trait>::AssetId,
-	{
-		/// Some assets were created.
-		Created(AssetId, Token<Balance>),
-		/// Some assets were issued.
-		Issued(AssetId, AccountId, Balance),
-		/// Some assets were transferred.
-		Transferred(AssetId, AccountId, AccountId, Balance),
-		/// Some assets were destroyed.
-		Destroyed(AssetId, AccountId, Balance),
-	}
-);
-
-// decl_storage! {
-// 	trait Store for Module<T: Trait> as Assets {
-// 		/// The number of units of assets held by any given asset ans given account.
-// 		pub Balances get(balances): map (T::AssetId, T::AccountId) => T::Balance;
-// 		/// The next asset identifier up for grabs.
-// 		NextAssetId get(next_asset_id): T::AssetId;
-// 		/// Details of the token corresponding to an asset id.
-// 		pub Tokens get(token_details): map T::AssetId => Token<T::Balance>;
-// 	}
-// }
-
-// decl_module! {
-// 	pub struct Module<T: Trait> for enum Call where origin: T::Origin {
-
-// 		fn deposit_event<T>() = default;
-
-// 		/// Create a new class of fungible assets. It will have an
-// 		/// identifier `AssetId` instance: this will be specified in the `Created` event.
-// 		pub fn create(origin, symbol: Vec<u8>, precision: u16) {
-// 			let _origin = ensure_root(origin)?;
-
-// 			ensure!(symbol.len() <= 32, "token symbol cannot exceed 32 bytes");
-// 			ensure!(precision <= 16, "token precision cannot exceed 16");
-
-// 			let id = Self::next_asset_id();
-// 			<NextAssetId<T>>::mutate(|id| *id += One::one());
-
-// 			// Initial total supply is zero.
-// 			let total_supply: T::Balance = 0.into();
-
-// 			let token = Token {
-// 				symbol: symbol.clone(),
-// 				precision: precision.clone(),
-// 				total_supply: total_supply,
-// 			};
-
-// 			<Tokens<T>>::insert(id, token.clone());
-
-// 			Self::deposit_event(RawEvent::Created(id, token));
-// 		}
-
-// 		/// Issue any amount of fungible assets.
-// 		pub fn issue(origin,
-// 			#[compact] id: T::AssetId,
-// 			target: <T::Lookup as StaticLookup>::Source,
-// 			#[compact] amount: T::Balance)
-// 		{
-// 			let _origin = ensure_root(origin)?;
-
-// 			ensure!(<Tokens<T>>::exists(&id), "asset should be created first");
-
-// 			let target = T::Lookup::lookup(target)?;
-// 			ensure!(!amount.is_zero(), "issue amount should be non-zero");
-
-// 			Self::asset_issue(id, target.clone(), amount);
-
-// 			Self::deposit_event(RawEvent::Issued(id, target, amount));
-// 		}
-
-// 		/// Move some assets from one holder to another.
-// 		pub fn transfer(origin,
-// 			#[compact] id: T::AssetId,
-// 			target: <T::Lookup as StaticLookup>::Source,
-// 			#[compact] amount: T::Balance)
-// 		{
-// 			let origin = ensure_signed(origin)?;
-// 			let origin_account = (id, origin.clone());
-// 			let origin_balance = <Balances<T>>::get(&origin_account);
-// 			let target = T::Lookup::lookup(target)?;
-// 			ensure!(!amount.is_zero(), "transfer amount should be non-zero");
-// 			ensure!(origin_balance >= amount,
-// 				"origin account balance must be greater than or equal to the transfer amount");
-
-// 			Self::asset_transfer(id, origin.clone(), target.clone(), amount);
-
-// 			Self::deposit_event(RawEvent::Transferred(id, origin, target, amount));
-// 		}
-
-// 		/// Destroy any amount of assets of `id` owned by `origin`.
-// 		pub fn destroy(origin, #[compact] id: T::AssetId, #[compact] amount: T::Balance) {
-// 			let origin = ensure_signed(origin)?;
-// 			let origin_account = (id, origin.clone());
-
-// 			let balance = <Balances<T>>::get(&origin_account);
-// 			ensure!(amount <= balance , "amount should be less than or equal to origin balance");
-
-// 			Self::asset_destroy(id, origin.clone(), amount);
-
-// 			Self::deposit_event(RawEvent::Destroyed(id, origin, amount));
-// 		}
-// 	}
-// }
-
-// impl<T: Trait> AssetIssue<T::AssetId, T::AccountId, T::Balance> for Module<T> {
-// 	fn asset_issue(asset_id: T::AssetId, target: T::AccountId, amount: T::Balance) {
-// 		Self::asset_issue(asset_id, target, amount);
-// 	}
-// }
-
-// The main implementation block for the module.
-// impl<T: Trait> Module<T> {
-	// fn asset_issue(
-	// 	asset_id: T::AssetId,
-	// 	target: T::AccountId,
-	// 	amount: T::Balance,
-	// ) {
-	// 	let now_block: T::BlockNumber = <system::Module<T>>::block_number();
-
-	// 	let target_asset = (asset_id, target.clone());
-	// 	let prev_target_balance = <Balances<T>>::get(&target_asset);
-	// 	let target_balance = <Balances<T>>::mutate(&target_asset, |balance| {
-	// 		*balance += amount;
-	// 		*balance
-	// 	});
-	// 	// T::ClearingHandler::asset_clearing(asset_id, target.clone(), now_block, prev_target_balance, target_balance);
-
-	// 	let prev_token = <Tokens<T>>::get(asset_id);
-	// 	let total_supply = <Tokens<T>>::mutate(asset_id, |token| {
-	// 		token.total_supply += amount;
-	// 		token.total_supply
-	// 	});
-	// 	// T::ClearingHandler::token_clearing(asset_id, now_block, prev_token.total_supply, total_supply);
-	// }
-
-	// fn asset_transfer(
-	// 	asset_id: T::AssetId,
-	// 	from: T::AccountId,
-	// 	to: T::AccountId,
-	// 	amount: T::Balance,
-	// ) {
-	// 	let now_block: T::BlockNumber = <system::Module<T>>::block_number();
-
-	// 	let from_asset = (asset_id, from.clone());
-	// 	let prev_from_balance = <Balances<T>>::get(&from_asset);
-	// 	let from_balance = <Balances<T>>::mutate(&from_asset, |balance| {
-	// 		*balance -= amount;
-	// 		*balance
-	// 	});
-	// 	// T::ClearingHandler::asset_clearing(asset_id, from.clone(), now_block, prev_from_balance, from_balance);
-
-	// 	let to_asset = (asset_id, to.clone());
-	// 	let prev_to_balance = <Balances<T>>::get(&to_asset);
-	// 	let to_balance = <Balances<T>>::mutate(&to_asset, |balance| {
-	// 		*balance += amount;
-	// 		*balance
-	// 	});
-	// 	// T::ClearingHandler::asset_clearing(asset_id, to.clone(), now_block, prev_to_balance, to_balance);
-	// }
-
-	// fn asset_destroy(
-	// 	asset_id: T::AssetId,
-	// 	target: T::AccountId,
-	// 	amount: T::Balance,
-	// ) {
-	// 	let now_block: T::BlockNumber = <system::Module<T>>::block_number();
-
-	// 	let target_asset = (asset_id, target.clone());
-	// 	let prev_target_balance = <Balances<T>>::get(&target_asset);
-	// 	let target_balance = <Balances<T>>::mutate(&target_asset, |balance| {
-	// 		*balance -= amount;
-	// 		*balance
-	// 	});
-	// 	// T::ClearingHandler::asset_clearing(asset_id, target.clone(), now_block, prev_target_balance, target_balance);
-
-	// 	let prev_token = <Tokens<T>>::get(&asset_id);
-	// 	let total_supply = <Tokens<T>>::mutate(asset_id, |token| {
-	// 		token.total_supply -= amount;
-	// 		token.total_supply
-	// 	});
-	// 	// T::ClearingHandler::token_clearing(asset_id, now_block, prev_token.total_supply, total_supply);
-	// }
-// }
---
+++ dna/assets/src/mint.rs
@@ -0,0 +1,138 @@
+// Copyright 2019 by Trinkler Software AG (Switzerland).
+// This file is part of the Katal Chain.
+//
+// Katal Chain is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version <http://www.gnu.org/licenses/>.
+//
+// Katal Chain is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+use super::*;
+
+// This function creates tokens of a given asset and deposits them into an address. If the recipient address doesn't exist, it is created.
+impl<T: Trait> Module<T> {
+    pub fn mint(to_address: H256, asset_id: u32, amount: Real) -> Result {
+        // Checking that amount is non-negative.
+        if amount < Real::from(0) {
+            return Err("Amount can't be negative.");
+        }
+
+        // Increasing supply.
+        if <Self as Store>::TotalSupply::exists(asset_id) {
+            let new_supply = <Self as Store>::TotalSupply::get(asset_id) + amount;
+            <Self as Store>::TotalSupply::insert(asset_id, new_supply);
+        } else {
+            <Self as Store>::TotalSupply::insert(asset_id, amount);
+        }
+
+        // Crediting amount to to_address.
+        if <Self as Store>::Balances::exists((asset_id, to_address)) {
+            let new_balance = <Self as Store>::Balances::get((asset_id, to_address)) + amount;
+            <Self as Store>::Balances::insert((asset_id, to_address), new_balance);
+        } else {
+            <Self as Store>::Balances::insert((asset_id, to_address), amount);
+        }
+
+        // Return Ok.
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use primitives::H256;
+    // The testing primitives are very useful for avoiding having to work with signatures
+    // or public keys. `u64` is used as the `AccountId` and no `Signature`s are required.
+    use sr_primitives::{
+        testing::Header,
+        traits::{BlakeTwo256, IdentityLookup},
+        Perbill,
+    };
+    use support::{assert_noop, assert_ok, impl_outer_origin, parameter_types};
+
+    impl_outer_origin! {
+        pub enum Origin for Test {}
+    }
+
+    // For testing the module, we construct most of a mock runtime. This means
+    // first constructing a configuration type (`Test`) which `impl`s each of the
+    // configuration traits of modules we want to use.
+    #[derive(Clone, Eq, PartialEq)]
+    pub struct Test;
+    parameter_types! {
+        pub const BlockHashCount: u64 = 250;
+        pub const MaximumBlockWeight: u32 = 1024;
+        pub const MaximumBlockLength: u32 = 2 * 1024;
+        pub const AvailableBlockRatio: Perbill = Perbill::one();
+    }
+    impl system::Trait for Test {
+        type Origin = Origin;
+        type Index = u64;
+        type Call = ();
+        type BlockNumber = u64;
+        type Hash = H256;
+        type Hashing = BlakeTwo256;
+        type AccountId = u64;
+        type Lookup = IdentityLookup<Self::AccountId>;
+        type Header = Header;
+        type Event = ();
+        type BlockHashCount = BlockHashCount;
+        type MaximumBlockWeight = MaximumBlockWeight;
+        type AvailableBlockRatio = AvailableBlockRatio;
+        type MaximumBlockLength = MaximumBlockLength;
+        type Version = ();
+    }
+    impl Trait for Test {
+        // If Events are ever added to this module, then the next line
+        // needs to be commented out.
+        // type Event = ();
+    }
+    // This next line should have the name of the module, in this
+    // case it is Assets
+    type Assets = Module<Test>;
+
+    // This function basically just builds a genesis storage key/value store according to
+    // our desired mockup.
+    fn new_test_ext() -> runtime_io::TestExternalities {
+        system::GenesisConfig::default()
+            .build_storage::<Test>()
+            .unwrap()
+            .into()
+    }
+
+    #[test]
+    fn mint_works() {
+        new_test_ext().execute_with(|| {
+            // Initialize some values.
+            let supply = Real::from(999000);
+            let to_address = H256::random();
+            let to_balance = Real::from(200);
+            let asset_id = 1;
+
+            // Manually store addresses with balances.
+            <Assets as Store>::TotalSupply::insert(asset_id, supply);
+            <Assets as Store>::Balances::insert((asset_id, to_address), to_balance);
+
+            // Test case of negative transfer amount.
+            let mut amount = Real::from(-100);
+            assert!(Assets::mint(to_address, asset_id, amount).is_err());
+
+            // Test normal case.
+            amount = Real::from(1000);
+            assert!(Assets::mint(to_address, asset_id, amount).is_ok());
+            assert_eq!(
+                supply + amount,
+                <Assets as Store>::TotalSupply::get(asset_id)
+            );
+            assert_eq!(
+                to_balance + amount,
+                <Assets as Store>::Balances::get((asset_id, to_address))
+            );
+        });
+    }
+}

dffddaa3137aa2ec7c5b28b55e7bf65168443f3a
dhruvinparikh <dparikh@apigarage.com>
2019-12-05 22:50:30 -0500

    Revert "`WIP` working"
    This reverts commit 0175bc6c9822c03d2a3ce8490b863ce886941e05.

--- dna/src/main.rs
+++ dna/src/main.rs
@@ -1,17 +1,4 @@
-// Copyright 2019 by Trinkler Software AG (Switzerland).
-// This file is part of the Katal Chain.
-//
-// Katal Chain is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version <http://www.gnu.org/licenses/>.
-//
-// Katal Chain is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-
-//! Katal Chain Blockchain CLI library.
+//! DNA Chain Blockchain CLI library.
 
 #![warn(missing_docs)]
 #![warn(unused_extern_crates)]
@@ -30,9 +17,9 @@ fn main() -> Result<(), cli::error::Error> {
         commit: env!("VERGEN_SHA_SHORT"),
         version: env!("CARGO_PKG_VERSION"),
         executable_name: "dnachain",
-        author: "kush, dhruvin, jesse",
+        author: "kpatel, dhruvin, jesse",
         description: "Cryptocurrency Movement, Standardized.",
-        support_url: "https://github.com/blockxlabs/blockchain/issues/new",
+        support_url: "https://github.com/blockxlabs/dna/issues/new",
     };
 
     cli::run(std::env::args(), cli::Exit, version)

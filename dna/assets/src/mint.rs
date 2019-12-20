8871eab6e6b33ec0c9b5769b85deb0af8f51caad
kushptlapigarage <kpatel@apigarage.com>
2019-12-20 15:17:42 -0500

    Compiles

--- dna/assets/src/mint.rs
+++ dna/assets/src/mint.rs
@@ -1,16 +1,3 @@
-// Copyright 2019 by Trinkler Software AG (Switzerland).
-// This file is part of the Katal Chain.
-//
-// Katal Chain is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version <http://www.gnu.org/licenses/>.
-//
-// Katal Chain is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-
 use super::*;
 
 // This function creates tokens of a given asset and deposits them into an address. If the recipient address doesn't exist, it is created.

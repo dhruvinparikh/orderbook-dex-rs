960dd306407873d42335fc6292024877fecf7a3c
dhruvinparikh <dparikh@apigarage.com>
2020-01-02 17:39:18 -0500

    `WIP` running dna nodes

--- scripts/start/node1.sh
+++ scripts/start/node1.sh
@@ -6,6 +6,6 @@ mkdir /tmp/acc1
 mkdir /tmp/acc1/chains
 mkdir /tmp/acc1/chains/dna_testnet
 mkdir /tmp/acc1/chains/dna_testnet/keystore
-cp -a $path/keystore/ /tmp/acc1/chains/dna_testnet/keystore/
+cp -a $path/keystore1/ /tmp/acc1/chains/dna_testnet/keystore/
 
-$path/target/debug/dnachain --validator -d /tmp/acc1 --bootnodes /ip4/192.168.0.13/tcp/3033/p2p/Qmece3bstSKgRomhPcAWswQMUFT3GRL5XpCuy8bFDggrwV --chain $path/dna1.json 
+$path/target/debug/dnachain --validator -d /tmp/acc1 --bootnodes /ip4/192.168.0.13/tcp/3033/p2p/Qmece3bstSKgRomhPcAWswQMUFT3GRL5XpCuy8bFDggrwV --chain $path/dnachainspec.json 
--- scripts/start/node2.sh
+++ scripts/start/node2.sh
@@ -6,6 +6,6 @@ mkdir /tmp/acc2
 mkdir /tmp/acc2/chains
 mkdir /tmp/acc2/chains/dna_testnet
 mkdir /tmp/acc2/chains/dna_testnet/keystore
-cp -a $path/keystore/ /tmp/acc2/chains/dna_testnet/keystore/
+cp -a $path/keystore2/ /tmp/acc2/chains/dna_testnet/keystore/
 
-$path/target/debug/dnachain --validator -d /tmp/acc2 --bootnodes /ip4/192.168.0.13/tcp/3033/p2p/Qmece3bstSKgRomhPcAWswQMUFT3GRL5XpCuy8bFDggrwV --chain $path/dna1.json
+$path/target/debug/dnachain --validator --bootnodes /ip4/192.168.0.13/tcp/3033/p2p/Qmece3bstSKgRomhPcAWswQMUFT3GRL5XpCuy8bFDggrwV -d /tmp/acc2 --chain $path/dnachainspec.json

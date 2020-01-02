93f162f5ef846afc9ff20ba04e9e56d8a7931e34
dhruvinparikh <imsilent93@gmail.com>
2020-01-02 15:46:22 -0500

    `WIP` dna node scripts

--- scripts/purge-chain.sh
+++ scripts/purge-chain.sh
@@ -1,5 +1,6 @@
+#!/usr/bin/env bash
 path=$(pwd)
 
-$path/target/debug/dnachain purge-chain -d /tmp/node0 -y
-$path/target/debug/dnachain purge-chain -d /tmp/node1 -y
-$path/target/debug/dnachain purge-chain -d /tmp/node2 -y
+$path/target/debug/dnachain purge-chain -d /tmp/acc0 -y
+$path/target/debug/dnachain purge-chain -d /tmp/acc1 -y
+$path/target/debug/dnachain purge-chain -d /tmp/acc2 -y

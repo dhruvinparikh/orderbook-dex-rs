218ad347fe736b2695d0dab97c793aa995015446
dhruvinparikh <imsilent93@gmail.com>
2020-01-02 17:39:18 -0500

    `WIP` running dna nodes

--- scripts/start/node0.sh
+++ scripts/start/node0.sh
@@ -6,6 +6,6 @@ mkdir /tmp/acc0
 mkdir /tmp/acc0/chains
 mkdir /tmp/acc0/chains/dna_testnet
 mkdir /tmp/acc0/chains/dna_testnet/keystore
-cp -a $path/keystore/ /tmp/acc0/chains/dna_testnet/keystore/
+cp -a $path/keystore0/ /tmp/acc0/chains/dna_testnet/keystore/
 
-$path/target/debug/dnachain --validator --node-key 92ea498a16084a1e88abf1b3c31b03a545ee608bc2686e64eeb670a237ad427c -d /tmp/acc0 --chain $path/dna1.json
+$path/target/debug/dnachain --validator --node-key 92ea498a16084a1e88abf1b3c31b03a545ee608bc2686e64eeb670a237ad427c -d /tmp/acc0 --chain $path/dnachainspec.json

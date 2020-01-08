e6676a9301c09859077b201706bb178cbede18ee
Dhruvin Parikh <dhruvinparikh@APIs-MacBook-Pro.local>
2020-01-08 10:36:52 -0500

    `WIP` modify node starter shell script

--- scripts/start/node0.sh
+++ scripts/start/node0.sh
@@ -2,10 +2,7 @@
 
 path=$(pwd)
 
-mkdir /tmp/acc0
-mkdir /tmp/acc0/chains
-mkdir /tmp/acc0/chains/dna_testnet
-mkdir /tmp/acc0/chains/dna_testnet/keystore
-cp -a $path/keystore0/ /tmp/acc0/chains/dna_testnet/keystore/
+mkdir -p /tmp/acc0/chains/dna_testnet/keystore
+cp -a $path/../metaverse-keystore/keystore0/ /tmp/acc0/chains/dna_testnet/keystore/
 
-$path/target/debug/dnachain --validator --name "DNA NODE 0" --node-key 92ea498a16084a1e88abf1b3c31b03a545ee608bc2686e64eeb670a237ad427c -d /tmp/acc0 --chain $path/dnachainspec.json
+$path/target/debug/dnachain --validator --name "DNA NODE 0" --node-key 92ea498a16084a1e88abf1b3c31b03a545ee608bc2686e64eeb670a237ad427c -d /tmp/acc0 --chain $path/build-spec.dna.json --rpc-cors all
--- scripts/start/node1.sh
+++ scripts/start/node1.sh
@@ -2,10 +2,7 @@
 
 path=$(pwd)
 
-mkdir /tmp/acc1
-mkdir /tmp/acc1/chains
-mkdir /tmp/acc1/chains/dna_testnet
-mkdir /tmp/acc1/chains/dna_testnet/keystore
-cp -a $path/keystore1/ /tmp/acc1/chains/dna_testnet/keystore/
+mkdir -p /tmp/acc1/chains/dna_testnet/keystore
+cp -a $path/../metaverse-keystore/keystore1/ /tmp/acc1/chains/dna_testnet/keystore/
 
-$path/target/debug/dnachain --validator --name "DNA NODE 1" -d /tmp/acc1 --bootnodes /ip4/192.168.0.13/tcp/3033/p2p/Qmece3bstSKgRomhPcAWswQMUFT3GRL5XpCuy8bFDggrwV --chain $path/dnachainspec.json 
+$path/target/debug/dnachain --validator --name "DNA NODE 1" -d /tmp/acc1 --bootnodes /ip4/192.168.0.13/tcp/3033/p2p/Qmece3bstSKgRomhPcAWswQMUFT3GRL5XpCuy8bFDggrwV --chain $path/build-spec.dna.json 

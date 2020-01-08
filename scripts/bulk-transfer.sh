93c3135a1a082812d0da56df1d30d06213a09b2f
dhruvinparikh <dparikh@apigarage.com>
2020-01-08 16:02:17 -0500

    pre-fund the test accounts

--- scripts/bulk-transfer.sh
+++ scripts/bulk-transfer.sh
@@ -2,11 +2,6 @@
 
 path=$(pwd)
 
-cd $path/scripts/transfer && npm install && cd $path
-if [ $# -eq 0 ]
-then
-node $path/scripts/transfer/bulk-transfer.js 
-else
-node $path/scripts/transfer/bulk-transfer.js $1 $2
-fi
+# cd $path/scripts/transfer && npm install && cd $pat
+node $path/scripts/transfer/bulk-transfer.js $@
 

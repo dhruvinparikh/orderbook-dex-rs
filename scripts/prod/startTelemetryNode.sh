ba11385a0beb82f01e2a0c882594d87351a1a797
dhruvinparikh <imsilent93@gmail.com>
2020-01-09 10:56:20 -0500

    start telemetry

---
+++ scripts/prod/startTelemetryNode.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+dnachain --chain /dnachain/build-spec.dna.json  --port 30333 -d /dnachain/chain/data-acc0 --ws-port 9944 --rpc-external --ws-external --rpc-cors all --bootnodes /ip4/142.93.151.164/tcp/3033/p2p/Qmece3bstSKgRomhPcAWswQMUFT3GRL5XpCuy8bFDggrwV --telemetry-url ws://telemetry.mvsdna.com:8000/submit

05aba4590d300afa3fa372a377814ae6ad609316
dhruvinparikh <dparikh@apigarage.com>
2020-01-10 07:56:42 -0500

    setting env

--- scripts/start/node2.sh
+++ scripts/start/node2.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -e
+
 if [ "$1" == "--env" -a "$2" == "production" ]; then
 kspath=/dnachain/metaverse-keystore/keystore2
 acc=acc2
@@ -10,5 +12,5 @@ else
 path=$(pwd)
 mkdir -p /tmp/acc2/chains/dna_testnet/keystore
 cp -a $path/../metaverse-keystore/keystore2/ /tmp/acc2/chains/dna_testnet/keystore/
-$path/target/debug/dnachain --validator --ws-port 9946 --port 3035 --name "DNA NODE 2" --bootnodes /ip4/192.168.0.13/tcp/3033/p2p/Qmece3bstSKgRomhPcAWswQMUFT3GRL5XpCuy8bFDggrwV -d /tmp/acc2 --chain $path/build-spec.dna.json --telemetry-url ws://localhost:8000/submit
+$path/target/debug/dnachain --validator --ws-port 9946 --port 3035 --name "DNA NODE 2" --bootnodes /ip4/192.168.0.13/tcp/3033/p2p/Qmece3bstSKgRomhPcAWswQMUFT3GRL5XpCuy8bFDggrwV -d /tmp/acc2 --chain $path/build-spec.dna.json --telemetry-url ws://telemetry.mvsdna.com:8000/submit
 fi

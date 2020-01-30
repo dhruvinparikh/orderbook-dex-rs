a23ba62a2a73ab923f0997255c3531cd262fe823
dhruvinparikh <imsilent93@gmail.com>
2020-01-29 19:38:19 -0500

    `WIP` staking working

--- node/runtime/src/constants.rs
+++ node/runtime/src/constants.rs
@@ -3,29 +3,28 @@
 /// Money matters.
 pub mod currency {
     use node_primitives::Balance;
-	pub const DNA: Balance = 10_000;
-	pub const DOLLARS: Balance = DNA;
-	pub const CENTS: Balance = DOLLARS / 100;
-	pub const MILLICENTS: Balance = CENTS / 1_000;
+    pub const DNA: Balance = 10_000;
+    pub const DOLLARS: Balance = DNA;
+    pub const CENTS: Balance = DOLLARS / 100;
+    pub const MILLICENTS: Balance = CENTS / 1_000;
 }
 
 /// Time.
 pub mod time {
-    use node_primitives::{Moment, BlockNumber};
+    use node_primitives::{BlockNumber, Moment};
 
-	pub const MILLISECS_PER_BLOCK: Moment = 6000;
-	pub const SLOT_DURATION: Moment = MILLISECS_PER_BLOCK;
-	pub const EPOCH_DURATION_IN_BLOCKS: BlockNumber = 1 * HOURS;
+    pub const MILLISECS_PER_BLOCK: Moment = 6000;
+    pub const SLOT_DURATION: Moment = MILLISECS_PER_BLOCK;
+    pub const EPOCH_DURATION_IN_BLOCKS: BlockNumber = 1 * HOURS;
 
-	// These time units are defined in number of blocks.
-	pub const MINUTES: BlockNumber = 60_000 / (MILLISECS_PER_BLOCK as BlockNumber);
-	pub const HOURS: BlockNumber = MINUTES * 60;
-	pub const DAYS: BlockNumber = HOURS * 24;
+    // These time units are defined in number of blocks.
+    pub const MINUTES: BlockNumber = 60_000 / (MILLISECS_PER_BLOCK as BlockNumber);
+    pub const HOURS: BlockNumber = MINUTES * 60;
+    pub const DAYS: BlockNumber = HOURS * 24;
 
-	// 1 in 4 blocks (on average, not counting collisions) will be primary babe blocks.
-	pub const PRIMARY_PROBABILITY: (u64, u64) = (1, 4);
+    // 1 in 4 blocks (on average, not counting collisions) will be primary babe blocks.
+    pub const PRIMARY_PROBABILITY: (u64, u64) = (1, 4);
 }
-
 
 // CRITICAL NOTE: The system module maintains two constants: a _maximum_ block weight and a
 // _ratio_ of it yielding the portion which is accessible to normal transactions (reserving the rest

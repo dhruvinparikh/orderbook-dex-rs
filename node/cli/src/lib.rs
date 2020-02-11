972dd2e96d60602a7bcf2d41032638643e6b8d98
dhruvinparikh <imsilent93@gmail.com>
2020-02-11 17:23:05 -0500

    `WIP` create local testnet spec

--- node/cli/src/lib.rs
+++ node/cli/src/lib.rs
@@ -1,12 +1,12 @@
 //! Console line interface.
 
+use log::info;
 pub use sc_cli::VersionInfo;
+use sc_cli::{error, IntoExit};
+use sc_cli::{parse_and_prepare, NoCustom, ParseAndPrepare};
+use sc_service::{AbstractService, Configuration, Roles as ServiceRoles};
 use tokio::prelude::Future;
 use tokio::runtime::{Builder as RuntimeBuilder, Runtime};
-use sc_cli::{parse_and_prepare, NoCustom, ParseAndPrepare};
-use sc_service::{AbstractService, Roles as ServiceRoles, Configuration};
-use sc_cli::{IntoExit, error};
-use log::info;
 
 mod chain_spec;
 use chain_spec::load_spec;
@@ -15,70 +15,78 @@ use chain_spec::load_spec;
 mod service;
 
 /// Parse command line arguments into service configuration.
-pub fn run<I, T, E>(args: I, exit: E, version: VersionInfo) -> error::Result<()> where
+pub fn run<I, T, E>(args: I, exit: E, version: VersionInfo) -> error::Result<()>
+where
     I: IntoIterator<Item = T>,
     T: Into<std::ffi::OsString> + Clone,
     E: IntoExit,
 {
     type Config<A, B> = Configuration<(), A, B>;
 
-    match parse_and_prepare::<NoCustom, NoCustom, _>(&version, "Metaverse-DNA-by-Blockx-Labs", args) {
-        ParseAndPrepare::Run(cmd) => cmd.run(load_spec, exit,
-        |exit, _cli_args, _custom_args, config: Config<_, _>| {
-            info!("{}", version.name);
-            info!("  version {}", config.full_version());
-            info!("  by {}, 2018, 2019", version.author);
-            info!("Chain specification: {}", config.chain_spec.name());
-            info!("Node name: {}", config.name);
-            info!("Roles: {:?}", config.roles);
-            // TODO: We might ont need
-            let runtime = RuntimeBuilder::new().name_prefix("main-tokio-").build()
-                .map_err(|e| format!("{:?}", e))?;
-            match config.roles {
-                ServiceRoles::LIGHT => run_until_exit(
-                    runtime,
-                    service::new_light(config)?,
-                    exit
-                ),
-                _ => run_until_exit(
-                    runtime,
-                    service::new_full(config)?,
-                    exit
-                ),
-            }
-        }),
+    match parse_and_prepare::<NoCustom, NoCustom, _>(&version, "Metaverse-DNA-by-Blockx-Labs", args)
+    {
+        ParseAndPrepare::Run(cmd) => cmd.run(
+            load_spec,
+            exit,
+            |exit, _cli_args, _custom_args, config: Config<_, _>| {
+                info!("{}", version.name);
+                info!("  version {}", config.full_version());
+                info!("  by {}, 2018, 2019", version.author);
+                info!("Chain specification: {}", config.chain_spec.name());
+                info!("Node name: {}", config.name);
+                info!("Roles: {:?}", config.roles);
+                // TODO: We might ont need
+                let runtime = RuntimeBuilder::new()
+                    .name_prefix("main-tokio-")
+                    .build()
+                    .map_err(|e| format!("{:?}", e))?;
+                match config.roles {
+                    ServiceRoles::LIGHT => {
+                        run_until_exit(runtime, service::new_light(config)?, exit)
+                    }
+                    _ => run_until_exit(runtime, service::new_full(config)?, exit),
+                }
+            },
+        ),
         ParseAndPrepare::BuildSpec(cmd) => cmd.run::<NoCustom, _, _, _>(load_spec),
-        ParseAndPrepare::ExportBlocks(cmd) => cmd.run_with_builder(|config: Config<_, _>|
-            Ok(new_full_start!(config).0), load_spec, exit),
-        ParseAndPrepare::ImportBlocks(cmd) => cmd.run_with_builder(|config: Config<_, _>|
-            Ok(new_full_start!(config).0), load_spec, exit),
-        ParseAndPrepare::CheckBlock(cmd) => cmd.run_with_builder(|config: Config<_, _>|
-            Ok(new_full_start!(config).0), load_spec, exit),
+        ParseAndPrepare::ExportBlocks(cmd) => cmd.run_with_builder(
+            |config: Config<_, _>| Ok(new_full_start!(config).0),
+            load_spec,
+            exit,
+        ),
+        ParseAndPrepare::ImportBlocks(cmd) => cmd.run_with_builder(
+            |config: Config<_, _>| Ok(new_full_start!(config).0),
+            load_spec,
+            exit,
+        ),
+        ParseAndPrepare::CheckBlock(cmd) => cmd.run_with_builder(
+            |config: Config<_, _>| Ok(new_full_start!(config).0),
+            load_spec,
+            exit,
+        ),
         ParseAndPrepare::PurgeChain(cmd) => cmd.run(load_spec),
-        ParseAndPrepare::RevertChain(cmd) => cmd.run_with_builder(|config: Config<_, _>|
-            Ok(new_full_start!(config).0), load_spec),
+        ParseAndPrepare::RevertChain(cmd) => cmd.run_with_builder(
+            |config: Config<_, _>| Ok(new_full_start!(config).0),
+            load_spec,
+        ),
         ParseAndPrepare::CustomCommand(_) => Ok(()),
     }
 }
 
-fn run_until_exit<T, E>(
-    mut runtime: Runtime,
-    service: T,
-    e: E,
-) -> error::Result<()>
-    where
-        T: AbstractService,
-        E: IntoExit,
+fn run_until_exit<T, E>(mut runtime: Runtime, service: T, e: E) -> error::Result<()>
+where
+    T: AbstractService,
+    E: IntoExit,
 {
-    use futures::{FutureExt, TryFutureExt, channel::oneshot, future::select, compat::Future01CompatExt};
+    use futures::{
+        channel::oneshot, compat::Future01CompatExt, future::select, FutureExt, TryFutureExt,
+    };
 
     let (exit_send, exit) = oneshot::channel();
 
     let informant = sc_cli::informant::build(&service);
 
-    let future = select(informant, exit)
-        .map(|_| Ok(()))
-        .compat();
+    let future = select(informant, exit).map(|_| Ok(())).compat();
 
     runtime.executor().spawn(future);
 
@@ -88,12 +96,8 @@ fn run_until_exit<T, E>(
 
     let service_res = {
         let exit = e.into_exit();
-        let service = service
-            .map_err(|err| error::Error::Service(err))
-            .compat();
-        let select = select(service, exit)
-            .map(|_| Ok(()))
-            .compat();
+        let service = service.map_err(|err| error::Error::Service(err)).compat();
+        let select = select(service, exit).map(|_| Ok(())).compat();
         runtime.block_on(select)
     };
 
--- scripts/purge-chain.sh
+++ scripts/purge-chain.sh
@@ -2,13 +2,13 @@
 path=$(pwd)
 
 if [ "$1" == "--env" -a "$2" == "production" ]; then
-dnachain purge-chain -d /dnachain/chain/acc0 -y 
-dnachain purge-chain -d /dnachain/chain/acc1 -y
-dnachain purge-chain -d /dnachain/chain/acc2 -y
-dnachain purge-chain -d /dnachain/chain/data-acc0 -y
+dnachain purge-chain --chain=dna -d /dnachain/chain/acc0 -y 
+dnachain purge-chain --chain=dna -d /dnachain/chain/acc1 -y
+dnachain purge-chain --chain=dna -d /dnachain/chain/acc2 -y
+dnachain purge-chain --chain=dna -d /dnachain/chain/data-acc0 -y
 else
-$path/target/debug/dnachain purge-chain -d /tmp/chain/acc0 -y
-$path/target/debug/dnachain purge-chain -d /tmp/chain/acc1 -y
-$path/target/debug/dnachain purge-chain -d /tmp/chain/acc2 -y
-$path/target/debug/dnachain purge-chain -d /tmp/chain/data-acc0 -y
+$path/target/debug/dnachain purge-chain --chain=local -d /tmp/chain/acc0 -y
+$path/target/debug/dnachain purge-chain --chain=local -d /tmp/chain/acc1 -y
+$path/target/debug/dnachain purge-chain --chain=local -d /tmp/chain/acc2 -y
+$path/target/debug/dnachain purge-chain --chain=local -d /tmp/chain/data-acc0 -y
 fi

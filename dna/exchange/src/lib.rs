f1c86e554f825cf7d50317d00af4e6c3ee9f1d10
dhruvinparikh <dparikh@apigarage.com>
2020-02-14 15:15:18 -0500

    create order

---
+++ dna/exchange/src/lib.rs
@@ -0,0 +1,45 @@
+//! # Assets module
+//!
+
+#![cfg_attr(not(feature = "std"), no_std)]
+// The above line is needed to compile the Wasm binaries.
+
+// Importing crates declared in the cargo.toml file.
+use codec::{Decode, Encode};
+use core::ops::{Add, AddAssign, Div, DivAssign, Mul, MulAssign, Sub, SubAssign};
+use primitives::H256;
+use rstd::fmt::Debug;
+use rstd::prelude::*;
+use support::{decl_module, decl_storage, dispatch::DispatchResult, StorageMap};
+use system::ensure_signed;
+
+
+// This module's storage items.
+decl_storage! {
+    trait Store for Module<T: Trait> as ExchangeStorage {
+    }
+}
+
+
+// This module's dispatchable functions.
+decl_module! {
+    // The module declaration.
+    pub struct Module<T: Trait> for enum Call where origin: T::Origin {
+
+        /// # Provide info to create an order limit
+        /// * `_origin` - Owner of the asset
+        /// * `base` - hash of base asset
+        /// * `quote` - hash of quote asset
+        /// * `price` - price per unit of the base unit
+        /// * `sell_amount` -  amount kept for trade
+        /// * `order_type` - buy or sell
+        pub fn create_order_limit(_origin, base:H256, quote:H256, price:u32,sell_amount:Balance,order_type:OrderType) -> DispatchResult {
+            let sender = ensure_signed(_origin)?;
+
+             // call corresponding internal function
+
+             // Return Ok if successful.
+             Ok(())
+        }
+    }
+}

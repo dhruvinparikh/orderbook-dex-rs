7d086fd9172a1f6267bb5487549b7f5c25ae6197
dhruvinparikh <dparikh@apigarage.com>
2020-02-14 15:15:38 -0500

    define storage

--- dna/exchange/src/lib.rs
+++ dna/exchange/src/lib.rs
@@ -17,6 +17,16 @@ use system::ensure_signed;
 // This module's storage items.
 decl_storage! {
     trait Store for Module<T: Trait> as ExchangeStorage {
+        ///	TradePairHash => TradePair
+		TradePairs get(fn trade_pair): map T::Hash => Option<TradePair<T>>;
+
+		/// OrderHash => Order
+		Orders get(fn order): map T::Hash => Option<LimitOrder<T>>;
+
+		/// TradeHash => Trade
+		Trades get(fn trade): map T::Hash => Option<Trade<T>>;
+
+		Nonce: u64;
     }
 }
 
@@ -27,13 +37,26 @@ decl_module! {
     pub struct Module<T: Trait> for enum Call where origin: T::Origin {
 
         /// # Provide info to create an order limit
-        /// * `_origin` - Owner of the asset
-        /// * `base` - hash of base asset
-        /// * `quote` - hash of quote asset
+        /// * `_origin` - signer
+        /// * `base` - hash/asset_id of base asset
+        /// * `quote` - hash/asset_id of quote asset
         /// * `price` - price per unit of the base unit
         /// * `sell_amount` -  amount kept for trade
         /// * `order_type` - buy or sell
         pub fn create_order_limit(_origin, base:H256, quote:H256, price:u32,sell_amount:Balance,order_type:OrderType) -> DispatchResult {
+            let sender = ensure_signed(_origin)?;
+
+             // call corresponding internal function
+
+             // Return Ok if successful.
+             Ok(())
+        }
+
+        /// # Provide info to create a trade pair
+        /// * `_origin` - signer
+        /// * `base` - hash/asset_id of base asset
+        /// * `quote` - hash/asset_id of quote asset
+        pub fn create_trade_pair(_origin, base:H256, quote:H256) -> DispatchResult {
             let sender = ensure_signed(_origin)?;
 
              // call corresponding internal function

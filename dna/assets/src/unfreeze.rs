45acd32eb261b017436a47e7dc2502eb043dd720
dhruvinparikh <dparikh@apigarage.com>
2020-02-19 12:18:48 -0500

    `WIP` freeze and unfreeze assets

---
+++ dna/assets/src/unfreeze.rs
@@ -0,0 +1,30 @@
+use super::*;
+
+// This function unfreezes assets of a specific address.
+impl<T: Trait> Module<T> {
+    pub fn unfreeze(sender: T::AccountId, hash: T::Hash, amount: T::Balance) -> DispatchResult {
+        let asset = Self::asset(hash);
+        ensure!(asset.is_some(), Error::<T>::NoMatchingAsset);
+
+        ensure!(
+            FreeBalanceOf::<T>::exists((sender.clone(), hash)),
+            Error::<T>::SenderHaveNoAsset
+        );
+
+        let old_freezed_amount = Self::freezed_balance_of((sender.clone(), hash.clone()));
+        ensure!(old_freezed_amount >= amount, Error::<T>::BalanceNotEnough);
+
+        let old_free_amount = Self::free_balance_of((sender.clone(), hash.clone()));
+        ensure!(
+            old_free_amount + amount <= T::Balance::max_value(),
+            Error::<T>::AmountOverflow
+        );
+
+        FreeBalanceOf::<T>::insert((sender.clone(), hash.clone()), old_free_amount + amount);
+        FreezedBalanceOf::<T>::insert((sender.clone(), hash.clone()), old_freezed_amount - amount);
+
+        Self::deposit_event(RawEvent::UnFreezed(sender, hash, amount));
+
+        Ok(())
+    }
+}

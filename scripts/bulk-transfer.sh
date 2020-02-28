1a90004c078a1d14524b03d7508710549ad5b5d6
dhruvinparikh <imsilent93@gmail.com>
2020-02-28 14:38:27 -0500

    correct path

--- scripts/bulk-transfer.sh
+++ scripts/bulk-transfer.sh
@@ -5,18 +5,17 @@ path=$(pwd)
 cd $path/scripts/transfer
 rm -rf node_modules
 rm -rf package-lock.json
-npm clean
 if [ "$1" == "--env" -a "$2" == "jenkins" ]; then
 echo "Starting installing deps in jenkins box."
-cp -aRf ./kush/metaverse-dna-transfer/ .
+cp -aRf /home/robot/kush/metaverse-dna-transfer/ .
 else
 echo "Starting installing deps."
 fi
 npm install
 if [ "$1" == "--env" -a "$2" == "jenkins" ]; then
 echo "Finished installing dependencies in jenkins box."
-cp -aRf node_modules ./kush/metaverse-dna-transfer/
-cp -aRf package-lock.json ./kush/metaverse-dna-transfer/
+cp -aRf node_modules /home/robot/kush/metaverse-dna-transfer/
+cp -aRf package-lock.json /home/robot/kush/metaverse-dna-transfer/
 else
 echo "Finished installing deps."
 fi 

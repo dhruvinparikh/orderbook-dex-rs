c2dd98ea2bf8fc2fe379d38fa120fd87652eeafb
dhruvinparikh <imsilent93@gmail.com>
2020-02-28 19:40:14 +0000

    Merged in MT_Jenkins (pull request #68)
    MT Jenkins
    
    * removing job cacher
    
    * Merge branch 'master' of https://bitbucket.org/apigarage-core/metaverse-dna into MT_Jenkins
    
    * remove duplicate stage name
    
    * Merge branch 'master' of https://bitbucket.org/apigarage-core/metaverse-dna into MT_Jenkins
    
    * copy deps from bk up in jenkins
    
    * Merge branch 'master' of https://bitbucket.org/apigarage-core/metaverse-dna into MT_Jenkins
    
    * correct node_modules path
    
    * Merge branch 'master' of https://bitbucket.org/apigarage-core/metaverse-dna into MT_Jenkins
    
    * correct path
    
    * Merge branch 'master' of https://bitbucket.org/apigarage-core/metaverse-dna into MT_Jenkins

--- scripts/bulk-transfer.sh
+++ scripts/bulk-transfer.sh
@@ -8,15 +8,15 @@ rm -rf package-lock.json
 npm clean
 if [ "$1" == "--env" -a "$2" == "jenkins" ]; then
 echo "Starting installing deps in jenkins box."
-cp -aRf ./kush/metaverse-dna-transfer/ .
+cp -aRf /home/robot/kush/metaverse-dna-transfer/ .
 else
 echo "Starting installing deps."
 fi
 npm install
 if [ "$1" == "--env" -a "$2" == "jenkins" ]; then
 echo "Finished installing dependencies in jenkins box."
-cp -aRf node_modules ./kush/metaverse-dna-transfer/
-cp -aRf package-lock.json ./kush/metaverse-dna-transfer/
+cp -aRf node_modules /home/robot/kush/metaverse-dna-transfer/
+cp -aRf package-lock.json /home/robot/kush/metaverse-dna-transfer/
 else
 echo "Finished installing deps."
 fi 

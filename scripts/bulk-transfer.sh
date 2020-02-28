480da813225cdc5abdd8d4af4dab950b452c2964
Dhruvin Parikh <dparikh@apigarage.com>
2020-02-28 19:31:52 +0000

    Merged in MT_Jenkins (pull request #66)
    MT Jenkins
    
    * Merge branch 'master' of https://bitbucket.org/apigarage-core/metaverse-dna into MT_Jenkins
    
    * list cache
    
    * Merge branch 'master' of https://bitbucket.org/apigarage-core/metaverse-dna into MT_Jenkins
    
    * correct the cache path
    
    * Merge branch 'master' of https://bitbucket.org/apigarage-core/metaverse-dna into MT_Jenkins
    
    * removing job cacher
    
    * Merge branch 'master' of https://bitbucket.org/apigarage-core/metaverse-dna into MT_Jenkins
    
    * remove duplicate stage name
    
    * Merge branch 'master' of https://bitbucket.org/apigarage-core/metaverse-dna into MT_Jenkins
    
    * copy deps from bk up in jenkins

--- scripts/bulk-transfer.sh
+++ scripts/bulk-transfer.sh
@@ -5,7 +5,19 @@ path=$(pwd)
 cd $path/scripts/transfer
 rm -rf node_modules
 rm -rf package-lock.json
-npm install 
+if [ "$1" == "--env" -a "$2" == "jenkins" ]; then
+echo "Starting installing deps in jenkins box."
+cp -aRf ./kush/metaverse-dna-transfer/* .
+else
+echo "Starting installing deps."
+fi
+npm install
+if [ "$1" == "--env" -a "$2" == "jenkins" ]; then
+echo "Finished installing dependencies in jenkins box."
+cp -aRf ./kush/metaverse-dna-transfer/* .
+else
+echo "Finished installing deps."
+fi 
 cd $pat
 node $path/scripts/transfer/bulk-transfer.js $@
 
